e16dc9815c0ea8abb834c3c36bf6b9e59c576dd5
Uchchash346 <mru416@gmail.com>
2022-09-22 12:53:44 +0600

    Project setup, sign in and register page design

---
+++ student/register.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en" class="fixed accounts sign-in">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Student Registration</title>
+    <!--BASIC css-->
+    <!-- ========================================================= -->
+    <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="../assets/vendor/animate.css/animate.css">
+    <!--SECTION css-->
+    <!-- ========================================================= -->
+    <!--TEMPLATE css-->
+    <!-- ========================================================= -->
+    <link rel="stylesheet" href="../assets/stylesheets/css/style.css">
+</head>
+
+<body>
+    <div class="wrap">
+        <!-- page BODY -->
+        <!-- ========================================================= -->
+        <div class="page-body animated slideInDown">
+            <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+            <!--LOGO-->
+            <div class="logo">
+                <h1 class="text-center">LMS</h1>
+            </div>
+            <div class="box">
+                <!--SIGN IN FORM-->
+                <div class="panel mb-none">
+                    <div class="panel-content bg-scale-0">
+                        <form>
+                            <div class="form-group mt-md">
+                                <span class="input-with-icon">
+                                    <input type="text" class="form-control" id="name" placeholder="Name">
+                                    <i class="fa fa-user"></i>
+                                </span>
+                            </div>
+                            <div class="form-group mt-md">
+                                <span class="input-with-icon">
+                                    <input type="email" class="form-control" id="email" placeholder="Email">
+                                    <i class="fa fa-envelope"></i>
+                                </span>
+                            </div>
+                            <div class="form-group">
+                                <span class="input-with-icon">
+                                    <input type="password" class="form-control" id="password" placeholder="Password">
+                                    <i class="fa fa-key"></i>
+                                </span>
+                            </div>
+                            <div class="form-group">
+                                <span class="input-with-icon">
+                                    <input type="password" class="form-control" id="confirm-password" placeholder="Confirm Password">
+                                    <i class="fa fa-key"></i>
+                                </span>
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox-custom checkbox-primary">
+                                    <input type="checkbox" id="terms" value="option1">
+                                    <label class="check" for="terms">I agree </label> to the <a href="#">Terms and Conditions</a>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <a href="index.html" class="btn btn-primary btn-block">Register</a>
+                            </div>
+                            <div class="form-group text-center">
+                                Have an account?, <a href="pages_sign-in.html">Sign In</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+        </div>
+    </div>
+    <!--BASIC scripts-->
+    <!-- ========================================================= -->
+    <script src="../assets/vendor/jquery/jquery-1.12.3.min.js"></script>
+    <script src="../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../assets/vendor/nano-scroller/nano-scroller.js"></script>
+    <!--TEMPLATE scripts-->
+    <!-- ========================================================= -->
+    <script src="../assets/javascripts/template-script.min.js"></script>
+    <script src="../assets/javascripts/template-init.min.js"></script>
+    <!-- SECTION script and examples-->
+    <!-- ========================================================= -->
+</body>
+
+</html>
