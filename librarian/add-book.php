ea589a0215a7df93d46cd8d49850c9bfc6fd1072
Uchchash346 <mru416@gmail.com>
2022-09-26 19:43:40 +0600

    Add book menu in librarian side

---
+++ librarian/add-book.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'header.php';
+?>
+<!-- content HEADER -->
+<!-- ========================================================= -->
+<div class="content-header">
+    <!-- leftside content header -->
+    <div class="leftside-content-header">
+        <ul class="breadcrumbs">
+            <li><i class="fa fa-home" aria-hidden="true"></i><a href="index.php">Dashboard</a></li>
+            <li><a href="javascript:avoid(0)">Add Book</a></li>
+        </ul>
+    </div>
+</div>
+<!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+<div class="row animated fadeInUp">
+    <div class="col-sm-12">
+        test
+    </div>
+</div>
+
+<?php
+require_once 'footer.php';
+?>
