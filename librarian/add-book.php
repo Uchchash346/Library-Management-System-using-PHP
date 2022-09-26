f329b03aec3f08dda4193e8d7aa098024d52f860
Uchchash346 <mru416@gmail.com>
2022-09-26 19:58:23 +0600

    Setup add book form

--- librarian/add-book.php
+++ librarian/add-book.php
@@ -14,8 +14,42 @@ require_once 'header.php';
 </div>
 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
 <div class="row animated fadeInUp">
-    <div class="col-sm-12">
-        test
+    <div class="col-sm-6 col-sm-offset-3">
+        <div class="panel-content">
+            <div class="row">
+                <div class="col-md-12">
+                    <form class="form-horizontal">
+                        <h5 class="mb-lg">To enjoy more, sing in!</h5>
+                        <div class="form-group">
+                            <label for="email2" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email2" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="password2" class="col-sm-2 control-label">Password</label>
+                            <div class="col-sm-10">
+                                <input type="password" class="form-control" id="password2" placeholder="Password">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox"> Remember me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-2 col-sm-10">
+                                <button type="submit" class="btn btn-primary">Sign in</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
