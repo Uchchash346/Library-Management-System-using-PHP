2cd2ee296f7013cfd99a37a03c61001bd6f3f51e
Uchchash346 <mru416@gmail.com>
2022-09-27 11:28:34 +0600

    Design add book from with image functionality

--- librarian/add-book.php
+++ librarian/add-book.php
@@ -1,5 +1,21 @@
 <?php
 require_once 'header.php';
+
+if (isset($_POST['save_book'])) {
+    $book_name = $_POST['book_name'];
+    $book_author_name = $_POST['book_author_name'];
+    $book_purchase_date = $_POST['book_purchase_date'];
+    $book_price = $_POST['book_price'];
+    $book_qty = $_POST['book_qty'];
+    $available_qty = $_POST['available_qty'];
+
+    $image = explode('.', $_FILES['book_image']['name']);
+    $image_ext = end($image);
+
+    $image = date('Ymdhis.').$image_ext;
+    echo $image;
+}
+
 ?>
 <!-- content HEADER -->
 <!-- ========================================================= -->
@@ -18,32 +34,53 @@ require_once 'header.php';
         <div class="panel-content">
             <div class="row">
                 <div class="col-md-12">
-                    <form class="form-horizontal">
-                        <h5 class="mb-lg">To enjoy more, sing in!</h5>
+                    <form class="form-horizontal" action="" method="POST" enctype="multipart/form-data">
+                        <h5 class="mb-lg">Add Book</h5>
                         <div class="form-group">
-                            <label for="email2" class="col-sm-2 control-label">Email</label>
-                            <div class="col-sm-10">
-                                <input type="email" class="form-control" id="email2" placeholder="Email">
+                            <label for="book_name" class="col-sm-4 control-label">Book Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="book_name" name="book_name" placeholder="Book Name">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="password2" class="col-sm-2 control-label">Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" id="password2" placeholder="Password">
+                            <label for="book_image" class="col-sm-4 control-label">Book Image</label>
+                            <div class="col-sm-8">
+                                <input type="file" id="book_image" name="book_image">
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox"> Remember me
-                                    </label>
-                                </div>
+                            <label for="book_author_name" class="col-sm-4 control-label">Author Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="book_author_name" name="book_author_name" placeholder="Book Author Name">
                             </div>
                         </div>
                         <div class="form-group">
-                            <div class="col-sm-offset-2 col-sm-10">
-                                <button type="submit" class="btn btn-primary">Sign in</button>
+                            <label for="book_purchase_date" class="col-sm-4 control-label">Purchase Date</label>
+                            <div class="col-sm-8">
+                                <input type="date" class="form-control" id="book_purchase_date" name="book_purchase_date" placeholder="Book Name">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="book_price" class="col-sm-4 control-label">Book Price</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" id="book_price" name="book_price" placeholder="Book Price">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="book_qty" class="col-sm-4 control-label">Book Quantity</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" id="book_qty" name="book_qty" placeholder="Book Quantity">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="available_qty" class="col-sm-4 control-label">Available Quantity</label>
+                            <div class="col-sm-8">
+                                <input type="number" class="form-control" id="available_qty" name="available_qty" placeholder="Available Quantity">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-offset-4 col-sm-8">
+                                <button type="submit" name="save_book" class="btn btn-primary"><i class="fa fa-save"></i> Save Book</button>
                             </div>
                         </div>
                     </form>
