7bd3357a9dd932443ce72191ef60a7d480f983cf
Uchchash346 <mru416@gmail.com>
2022-09-27 14:10:44 +0600

    Display Book Information

--- librarian/manage-book.php
+++ librarian/manage-book.php
@@ -15,7 +15,49 @@ require_once 'header.php';
 <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
 <div class="row animated fadeInUp">
     <div class="col-sm-12">
-        test
+        <h4 class="section-subtitle"><b>Books</b></h4>
+        <div class="panel">
+            <div class="panel-content">
+                <div class="table-responsive">
+                    <table id="basic-table" class="data-table table table-striped nowrap table-hover table-bordered" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Book Name</th>
+                                <th>Book Image</th>
+                                <th>Author Name</th>
+                                <th>Publication Name</th>
+                                <th>Purchase Date</th>
+                                <th>Book Price</th>
+                                <th>Book Quantity</th>
+                                <th>Available Quantity</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $result = mysqli_query($con, "SELECT * FROM `books`");
+                            while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                                <tr>
+                                    <td><?= $row['book_name'] ?></td>
+                                    <td><img style="width: 50px;" src="../images/books/<?= $row['book_image'] ?>" alt=""></td>
+                                    <td><?= $row['book_author_name'] ?></td>
+                                    <td><?= $row['book_publication_name'] ?></td>
+                                    <td><?= date('d-M-Y', strtotime($row['book_purchase_date'])) ?></td>
+                                    <td><?= $row['book_price'] ?></td>
+                                    <td><?= $row['book_qty'] ?></td>
+                                    <td><?= $row['available_qty'] ?></td>
+                                    <td>1</td>
+                                </tr>
+                            <?php
+
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
