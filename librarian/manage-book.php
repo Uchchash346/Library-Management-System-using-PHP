a3a4af6476b2fbbc12a42514c1a34d8085de87ec
Uchchash346 <mru416@gmail.com>
2022-09-28 16:00:03 +0600

    Update book information

--- librarian/manage-book.php
+++ librarian/manage-book.php
@@ -49,7 +49,7 @@ require_once 'header.php';
                                     <td><?= $row['available_qty'] ?></td>
                                     <td>
                                         <a href="javascript:avoid(0)" class="btn btn-info" data-toggle="modal" data-target="#book-<?php $row['id'] ?>"><i class="fa fa-eye"></i></a>
-                                        <a href="javascript:avoid(0)" class="btn btn-warning" data-toggle="modal" data-target="#book-update-<?php $row['id'] ?>"><i class="fa fa-pencil"></i></a>
+                                        <a href="#" class="btn btn-warning" data-toggle="modal" data-target="#book-update-<?php $row['id'] ?>"><i class="fa fa-pencil"></i></a>
                                         <a href="delete.php?bookdelete=<?= base64_encode($row['id']) ?>" class="btn btn-danger" onclick="return confirm('Are you sure to delete?')"><i class="fa fa-trash"></i></a>
                                     </td>
                                 </tr>
@@ -63,6 +63,7 @@ require_once 'header.php';
         </div>
     </div>
 </div>
+
 <?php
 $result = mysqli_query($con, "SELECT * FROM `books`");
 while ($row = mysqli_fetch_assoc($result)) {
@@ -119,64 +120,81 @@ while ($row = mysqli_fetch_assoc($result)) {
     <?php
 }
     ?>
-</div>
+    </div>
+    <!-- ====================================== -->
+    <!-- Update Book -->
+    </div>
+    </div>
 
-<!-- Book Update -->
-
-<?php
-$result = mysqli_query($con, "SELECT * FROM `books`");
-while ($row = mysqli_fetch_assoc($result)) {
-?>
-    <!-- Modal -->
-    <div class="modal fade" id="book-update-<?php $row['id'] ?>" id="info-modal" tabindex="-1" role="dialog" aria-labelledby="modal-info-label">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header state modal-info">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="modal-info-label"><i class="fa fa-book"></i>Book Info</h4>
-                </div>
-                <div class="modal-body">
-                    <table class="table table-bordered">
-                        <tr>
-                            <th>Book Name</th>
-                            <td><?= $row['book_name'] ?></td>
-                        </tr>
-                        <tr>
-                            <th>Book Image</th>
-                            <td><img style="width: 40px; height: 40px;" src="../images/books/<?= $row['book_image'] ?>" alt=""></td>
-                        </tr>
-                        <tr>
-                            <th>Author Name</th>
-                            <td><?= $row['book_author_name'] ?></td>
-                        </tr>
-                        <tr>
-                            <th>Publication Name</th>
-                            <td><?= $row['book_publication_name'] ?></td>
-                        </tr>
-                        <tr>
-                            <th>Purchase Date</th>
-                            <td><?= date('d-M-Y', strtotime($row['book_purchase_date'])) ?></td>
-                        </tr>
-                        <tr>
-                            <th>Book Price</th>
-                            <td><?= $row['book_price'] ?></td>
-                        </tr>
-                        <tr>
-                            <th>Book Quantity</th>
-                            <td><?= $row['book_qty'] ?></td>
-                        </tr>
-                        <tr>
-                            <th>Available Quantity</th>
-                            <td><?= $row['available_qty'] ?></td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    <?php
+    $result = mysqli_query($con, "SELECT * FROM `books`");
+    while ($row = mysqli_fetch_assoc($result)) {
+        $id = $row['id'];
+        $book_info = mysqli_query($con, "SELECT * FROM `books` WHERE `id` = '$id'");
+        $book_info_row = mysqli_fetch_assoc($book_info);
+    ?>
+        <!-- Modal -->
+        <div class="modal fade" id="book-update-<?php $row['id'] ?>" id="info-modal" tabindex="-1" role="dialog" aria-labelledby="modal-info-label">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header state modal-info">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="modal-info-label"><i class="fa fa-book"></i>Update Book Info</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="panel">
+                            <div class="panel-content">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form method="post" action="">
+                                            <div class="form-group">
+                                                <label for="book_name">Book Name</label>
+                                                <input type="text" class="form-control" id="book_name" name="book_name" placeholder="Book Name" value="<?= $book_info_row['book_name'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_image">Book Image</label>
+                                                <input type="file" id="book_image" name="book_image" required>
+                                                <img style="width: 40px; height: 40px;" src="../images/books/<?= $book_info_row['book_image'] ?>" alt="">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_author_name">Author Name</label>
+                                                <input type="text" class="form-control" id="book_author_name" name="book_author_name" placeholder="Book Author Name" value="<?= $book_info_row['book_author_name'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_publication_name">Publication Name</label>
+                                                <input type="text" class="form-control" id="book_publication_name" name="book_publication_name" placeholder="Book Publication Name" value="<?= $book_info_row['book_publication_name'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_purchase_date">Purchase Date</label>
+                                                <input type="date" class="form-control" id="book_purchase_date" name="book_purchase_date" placeholder="Book Name" value="<?= $book_info_row['book_purchase_date'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_price">Book Price</label>
+                                                <input type="number" class="form-control" id="book_price" name="book_price" placeholder="Book Price" value="<?= $book_info_row['book_price'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="book_qty">Book Quantity</label>
+                                                <input type="number" class="form-control" id="book_qty" name="book_qty" placeholder="Book Quantity" value="<?= $book_info_row['book_qty'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="available_qty">Available Quantity</label>
+                                                <input type="number" class="form-control" id="available_qty" name="available_qty" placeholder="Available Quantity" value="<?= $book_info_row['available_qty'] ?>" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary"><i class="fa fa-save "></i> Update</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        <?php
+    }
+        ?>
         </div>
-    <?php
-}
-    ?>
-    <?php require_once 'footer.php'; ?>
+
+
+        <?php require_once 'footer.php'; ?>
