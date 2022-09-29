60892b5b46d47baf4bc940cbba5ea0f3db563f86
Uchchash346 <mru416@gmail.com>
2022-09-29 18:48:33 +0600

    Count total books quantity

--- librarian/index.php
+++ librarian/index.php
@@ -50,13 +50,19 @@ require_once 'header.php';
             <?php
             $books = mysqli_query($con, "SELECT * FROM `books`");
             $total_books = mysqli_num_rows($books);
+
+            $books_qty = mysqli_query($con, "SELECT SUM(`book_qty`) as total FROM `books`");
+            $qty = mysqli_fetch_assoc($books_qty);
+
+            $books_a_qty = mysqli_query($con, "SELECT SUM(`available_qty`) as total FROM `books`");
+            $qty_a = mysqli_fetch_assoc($books_a_qty);
             ?>
             <!--BOX Style 1-->
             <div class="col-sm-6 col-md-4 col-lg-3">
                 <div class="panel widgetbox wbox-1 bg-lighter-2 color-light">
-                    <a href="#">
+                    <a href="manage-book.php">
                         <div class="panel-content">
-                            <h1 class="title color-darker-2"> <i class="fa fa-book"></i> <?= $total_books ?> </h1>
+                            <h1 class="title color-darker-2"> <i class="fa fa-book"></i> <?= $total_books . ' (' . $qty['total'] . ')' . ' -' . $qty['total'] ?> </h1>
                             <h4 class="subtitle color-darker-1">Total Books</h4>
                         </div>
                     </a>
